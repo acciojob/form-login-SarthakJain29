11a0fcc96dc3d84e4a0b6b329318afe75be8408d
Sarthak Jain <93116216+SarthakJain29@users.noreply.github.com>
2025-05-30 20:29:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
-    //Write your code here
+    const fname = document.getElementsByName("fname")[0];
+	const lname = document.getElementsByName("lname")[0];
+
+	alert(fname.value +" " + lname.value);
 
 }
